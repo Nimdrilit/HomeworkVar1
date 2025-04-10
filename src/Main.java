9d4e776ba7de91fadda07b9c84a89e9f868b6b8d
Nimdrilit <nimdrilir@gmail.com>
2025-04-10 21:13:17 +0500

    Task 6

--- src/Main.java
+++ src/Main.java
@@ -33,5 +33,11 @@ public class Main {
     var weightBoxer1;
     var weightBoxer1 = 78.2;
     System.out.println("Вес первого боксера " + weightBoxer1 + "кг!");
-
+    var weightBoxer2;
+    var weightBoxer2 = 82.7;
+    System.out.println("Вес второго боксера " + weightBoxer2 + "кг!");
+    var totalWeight = weightBoxer1 + weightBoxer2;
+    System.out.println("Общий вес " + totalWeight + "кг!");
+    var weightDifference = weightBoxer2 - weightBoxer1;
+    System.out.println("Разница в весе между бойцами " + weightDifference + "кг!");
 }
