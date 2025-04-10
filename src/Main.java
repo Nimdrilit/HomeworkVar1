fbac3b3276733c07915435f8a5af6bf8d6c353d9
Nimdrilit <nimdrilir@gmail.com>
2025-04-10 21:08:22 +0500

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+    var dog;
+    var dog = 8.0;
+    System.out.println(dog);
+    var cat;
+    var cat = 3.6;
+    System.out.println(cat);
+    var paper;
+    var paper = 763789;
+    System.out.println(paper);
 
 }
